e1f22b10783ff7357931c1138a5b8499a631da8d
Hilary liu <2788370451@qq.com>
2025-04-08 19:36:26 +0800

    feat: Add pwa support and ahooks for develop

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
+import manifest from './manifest.json';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest,
+      registerType: 'autoUpdate',
+      devOptions: {
+        // 如果想在`vite dev`命令下调试PWA, 必须启用它
+        enabled: true,
+      },
+      workbox: { globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'] },
+    }),
+  ],
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
 });
